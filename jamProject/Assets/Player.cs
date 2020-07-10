d34134dfd3ff288eaa1e840ee61c9fb88563ecb8
Amir <amir200523@gmail.com>
2020-07-10 23:06:52 +0200

    game controlls and obstacles

---
+++ jamProject/Assets/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    private Vector3 targetPos;
+    public float distance;
+    public float speed;
+    public float maxDis;
+    public float minDis;
+
+    public float health = 3f;
+    public Text healthUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        healthUI.text = health.ToString();
+
+        if (health <= 0)
+        {
+            
+        }
+
+
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.D) && transform.position.x < maxDis)
+        {
+            targetPos = new Vector3(transform.position.x + distance, transform.position.y,transform.position.z);     
+
+        }
+        else if (Input.GetKeyDown(KeyCode.A) && transform.position.x > minDis)
+        {
+            targetPos = new Vector3(transform.position.x - distance, transform.position.y,transform.position.z);
+
+        }
+    }
+}
+
---
+++ jamProject/Assets/Spawner.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] Patterns;
+
+    public float tbtwSpawn;
+    public float startTbtwSpawn;
+    public float dtime;
+    public float minTime = 0.5f;
+    public Vector3 spawnPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tbtwSpawn = startTbtwSpawn;
+    }
+
+    void OnEnable()
+    {
+        StartCoroutine(SpawnRoutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator SpawnRoutine()
+    {
+        while (gameObject.activeInHierarchy)
+        {
+            // WAIT
+            float timeToWait = Mathf.Max(tbtwSpawn -= dtime, minTime);
+            yield return new WaitForSeconds(timeToWait);
+
+            // SPAWN
+            GameObject pattern = Patterns[Random.Range(0, Patterns.Length)];
+            Instantiate(pattern, spawnPos, Quaternion.identity);
+        }   
+    }
+}
