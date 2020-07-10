3971842955ab9feee9a34184b255fed93e5b17dc
Robin <robin.baum@protonmail.com>
2020-07-11 00:31:43 +0200

    Added Player Movment

--- jamProject/Assets/Player.cs
+++ jamProject/Assets/Player.cs
@@ -1,46 +1,38 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private Vector3 targetPos;
-    public float distance;
-    public float speed;
-    public float maxDis;
-    public float minDis;
+    public float movmentSpeed = 1.0F;
+    public float torqueSpeed = 1.0F;
 
-    public float health = 3f;
-    public Text healthUI;
+    Rigidbody2D rigidBody;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        healthUI.text = health.ToString();
 
-        if (health <= 0)
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.D))
         {
-            
+            rigidBody.AddTorque(-1 * Time.fixedDeltaTime * torqueSpeed);
         }
 
-
-        transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
-
-        if (Input.GetKeyDown(KeyCode.D) && transform.position.x < maxDis)
+        if (Input.GetKey(KeyCode.A))
         {
-            targetPos = new Vector3(transform.position.x + distance, transform.position.y,transform.position.z);     
-
+            rigidBody.AddTorque(1 * Time.fixedDeltaTime * torqueSpeed);
         }
-        else if (Input.GetKeyDown(KeyCode.A) && transform.position.x > minDis)
-        {
-            targetPos = new Vector3(transform.position.x - distance, transform.position.y,transform.position.z);
 
+        if (Input.GetKey(KeyCode.W))
+        {
+            rigidBody.AddForce(Time.fixedDeltaTime * movmentSpeed * transform.up);
         }
     }
 }
