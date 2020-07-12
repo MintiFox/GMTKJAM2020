6029c42c09503c7d8ec69bbb52810354065f00e6
Robin <robin.baum@protonmail.com>
2020-07-12 13:28:29 +0200

    Edited Power Up System

--- jamProject/Assets/Scripts/Power Ups/PowerUp.cs
+++ jamProject/Assets/Scripts/Power Ups/PowerUp.cs
@@ -5,48 +5,32 @@ using UnityEngine;
 
 public class PowerUp : MonoBehaviour
 {
-    public Component[] toDestroy;
+    [Header("Power Up Settings")]
     public float duaration;
-    public PowerUpManager manager;
     public bool autoRemove = true;
-
-    public float speed = 2.0F;
-    private bool collected;
 
     void Update()
     {
-        if (!collected)
-        {
-            transform.Translate(Vector3.up * Time.deltaTime * speed);
-        }
     }
 
-    void OnTriggerEnter2D(Collider2D other)
+    public void Activate()
     {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            collected = true;
-            foreach (Component component in toDestroy)
-            {
-                Destroy(component);
-            }
-            StartCoroutine(PowerUpRoutine());
-        }
+        StartCoroutine(PowerUpRoutine());
     }
 
     protected IEnumerator PowerUpRoutine()
     {
-        if (!manager.activated.ContainsKey(GetType()))
+        if (!PowerUpManager.instance.activated.ContainsKey(GetType()))
         {
-            manager.activated.Add(GetType(), 0);
+            PowerUpManager.instance.activated.Add(GetType(), 0);
         }
 
-        if (++manager.activated[GetType()] == 1)
+        if (++PowerUpManager.instance.activated[GetType()] == 1)
         {
             ApplyPowerUp();
         }
         yield return new WaitForSeconds(duaration);
-        if (autoRemove && (manager.activated[GetType()] = Math.Max(1, manager.activated[GetType()]) - 1) == 0)
+        if (autoRemove && (PowerUpManager.instance.activated[GetType()] = Math.Max(1, PowerUpManager.instance.activated[GetType()]) - 1) == 0)
         {
             RemovePowerUp();
         }
