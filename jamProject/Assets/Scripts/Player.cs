01580a80b6b6253bf7b50354681b9849dc043d31
Robin <robin.baum@protonmail.com>
2020-07-12 12:32:39 +0200

    Small Clean Up

---
+++ jamProject/Assets/Scripts/Player.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    
+    public float movmentSpeed = 1.0F;
+    public float torqueSpeed = 1.0F;
+    public float maxVelocity = 1.75F;
+    public float maxAngularVelocity = 400.0F;
+
+    public float spawnTime = 2.0F;
+
+    [Header("Walls")]
+    public GameObject up;
+    public GameObject down;
+    public GameObject left;
+    public GameObject right;
+    private Spawner[] spawner;
+
+    public static float VerticalSize
+    {
+        get
+        {
+            return Camera.main.orthographicSize;
+        }
+    }
+
+    public static float HorizontalSize
+    {
+        get
+        {
+            return Camera.main.orthographicSize * (Screen.width / (float)Screen.height);
+        }
+    }
+
+    private Rigidbody2D rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        InstantiateWalls();
+    }
+
+    void OnEnable()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    private IEnumerator Spawn()
+    {
+        while (isActiveAndEnabled)
+        {
+            yield return new WaitForSeconds(spawnTime);
+            StartCoroutine(spawner[UnityEngine.Random.Range(0, spawner.Length - 1)].Spawn());
+        }
+    }
+
+    private void InstantiateWalls()
+    {
+        Vector2 hPos = new Vector2(HorizontalSize + 0.5F, 0.0F);
+        Vector2 vPos = new Vector2(0.0F, VerticalSize + 0.5F);
+        Vector2 vSize = new Vector2(VerticalSize * 2.0F + 2.0F, 1.0F);
+        Vector2 hSize = new Vector2(HorizontalSize * 2.0F + 2.0F, 1.0F);
+
+        // IN
+        GameObject up = Instantiate(this.up, Vector3.zero, Quaternion.identity);
+        GameObject down = Instantiate(this.down, Vector3.zero, Quaternion.identity);
+        GameObject left = Instantiate(this.left, Vector3.zero, Quaternion.identity);
+        GameObject right = Instantiate(this.right, Vector3.zero, Quaternion.identity);
+
+        // SCALE
+        right.transform.localScale = left.transform.localScale = vSize;
+        down.transform.localScale = up.transform.localScale = hSize;
+
+        // PARENT
+        right.transform.parent = left.transform.parent = down.transform.parent = up.transform.parent = Camera.main.transform;
+
+        // MOVE
+        right.transform.localPosition = hPos;
+        left.transform.localPosition = -hPos;
+        up.transform.localPosition = vPos;
+        down.transform.localPosition = -vPos;
+        
+        // ROTATE
+        right.transform.localRotation = Quaternion.Euler(0.0F, 0.0F, 90F);
+        left.transform.localRotation = Quaternion.Euler(0.0F, 0.0F, -90F);
+        up.transform.localRotation = Quaternion.Euler(0.0F, 0.0F, 180F);
+        down.transform.localRotation = Quaternion.identity;
+
+        spawner = new Spawner[]
+        {
+            down.GetComponent<Spawner>(),
+            down.GetComponent<Spawner>(),
+            left.GetComponent<Spawner>(),
+            right.GetComponent<Spawner>()
+        };
+    }
+
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            rigidBody.AddTorque(-1 * Time.fixedDeltaTime * torqueSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rigidBody.AddTorque(1 * Time.fixedDeltaTime * torqueSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            rigidBody.AddForce(Time.fixedDeltaTime * movmentSpeed * transform.up);
+        }
+
+        rigidBody.velocity = new Vector2(Mathf.Clamp(rigidBody.velocity.x, -maxVelocity, maxVelocity), Mathf.Clamp(rigidBody.velocity.y, -maxVelocity, maxVelocity));
+        rigidBody.angularVelocity = Mathf.Clamp(rigidBody.angularVelocity, -maxAngularVelocity, maxAngularVelocity);
+    }
+}
+
---
+++ jamProject/Assets/Scripts/invincible.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class invincible : MonoBehaviour
+{
+    public float lastTimer = 10;
+
+    public bool InvIsActive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.J) && CoinBar.instance.currentCount >= 5 && lastTimer > 0)
+        {
+            lastTimer -= Time.deltaTime;
+            InvIsActive = true;
+        }
+        else
+        {
+            InvIsActive = false;
+        }
+    }
+
+}
