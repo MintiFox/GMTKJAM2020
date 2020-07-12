17262f8a2f2d9c593f04bded4c5d1724e472564f
Robin <robin.baum@protonmail.com>
2020-07-12 13:28:29 +0200

    Edited Power Up System for Better UI Integration

---
+++ jamProject/Assets/Scripts/Power Ups/PowerUpManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Messaging;
+using UnityEngine;
+
+public class PowerUpManager : MonoBehaviour
+{
+    public Dictionary<Type, uint> activated = new Dictionary<Type, uint>();
+
+    public static PowerUpManager instance;
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else { Destroy(this); }
+    }
+
+    public bool IsActivated(System.Type type)
+    {
+        return PowerUpManager.instance.activated.TryGetValue(type, out uint v) && v > 0;
+    }
+}
