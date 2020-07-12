14d85dd9525aaba4963252162fa31ed19e73cf93
MintiFox <38742674+MintiFox@users.noreply.github.com>
2020-07-11 20:54:48 -0500

    Changed the build target to webgl., added singleton to score counter
    also changed some build settings to accomodate for webgl build. Build tested and works!

--- jamProject/Assets/Sprites/ScoreCounter.cs
+++ jamProject/Assets/Sprites/ScoreCounter.cs
@@ -7,10 +7,21 @@ public class ScoreCounter : MonoBehaviour
 
     public TextMeshProUGUI text;
 
+    //how much score is added per second the game is running
     public float scorePerSecond;
+    //score rounded down
     public int score;
+    //score down to percent
     public float realScore;
-    
+
+    public static ScoreCounter instance;
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else { Destroy(this); }
+    }
+
     void FixedUpdate()
     {
         realScore += scorePerSecond * Time.fixedDeltaTime;
